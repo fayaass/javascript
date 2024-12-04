3aebd7794f053e154e88a96761caf03f74d53ab4
fayaass <muhammedfayas59504@gmail.com>
2024-12-04 09:04:28 +0530

    add

---
+++ javascript/demo.js
@@ -0,0 +1,51 @@
+// var a=10
+// var a=20
+// alert(a)
+
+// let a=10
+// a=20
+
+// const a=10
+// a=20
+
+// console.log(a);
+
+
+// var let const
+
+
+// let a=10.20
+// let a="welcome"
+// let a="123"
+// let a=[1,2,3,['abc']]
+// let a={name:"fayas",age:22}
+// let a=null  for null values
+// let a=false for false value
+
+// console.log(typeof a);
+
+
+// operators
+
+// arithmetic -,*,/,**,++,--,+
+
+// console.log(10+5)
+// console.log(10-5)
+// console.log(1085)
+
+// a=10
+// ++a
+// ++a
+// console.log(a)
+
+
+// assignment =,+=,-=,*=,/=,**+
+
+console.log(10=="10");
+console.log(10==="10")
+
+console.log(10!="11");
+console.log(10!="10");
+
+console.log(10==10 && 10==11); and
+console.log(10==10 || 10==11); or
