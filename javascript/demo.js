e597555408373d9a13fcd47868aef0dbeea042da
fayaass <muhammedfayas59504@gmail.com>
2024-12-05 09:01:29 +0530

    added

--- javascript/demo.js
+++ javascript/demo.js
@@ -41,11 +41,75 @@
 
 // assignment =,+=,-=,*=,/=,**+
 
-console.log(10=="10");
-console.log(10==="10")
+// console.log(10=="10");
+// console.log(10==="10")
 
-console.log(10!="11");
-console.log(10!="10");
+// console.log(10!="11");
+// console.log(10!="10");
 
-console.log(10==10 && 10==11); and
-console.log(10==10 || 10==11); or
+// console.log(10==10 && 10==11); and
+// console.log(10==10 || 10==11); or
+
+
+// function fun1(){
+
+
+        // Number
+
+    // let a=parseInt(document.getElementById("demo").value)
+    // let h3=document.getElementById("h3")
+    // h3.innerHTML=a
+
+         // text
+
+
+    // let a=document.getElementById("demo").value
+    // let h3=document.getElementById("h3")
+    // h3.innerHTML=a
+
+    // console.log(typeof a);
+    // console.log(a)
+
+    // console.log('welcome')
+
+// }
+
+function add(){
+    let a=parseInt(document.getElementById("add1").value)
+    let b=parseInt(document.getElementById("add2").value)
+    console.log(a+b);
+    let h3=document.getElementById("h3")
+    h3.innerHTML=a+b
+}
+
+function sub(){
+    let a=parseInt(document.getElementById("sub1").value)
+    let b=parseInt(document.getElementById("sub2").value)
+    console.log(a-b);
+    let h3=document.getElementById("h4")
+    h3.innerHTML=a-b
+}
+
+function mul(){
+    let a=parseInt(document.getElementById("mul1").value)
+    let b=parseInt(document.getElementById("mul2").value)
+    console.log(a*b);
+    let h3=document.getElementById("h5")
+    h3.innerHTML=a*b
+}
+
+function div(){
+    let a=parseInt(document.getElementById("div1").value)
+    let b=parseInt(document.getElementById("div2").value)
+    console.log(a/b);
+    let h3=document.getElementById("h6")
+    h3.innerHTML=a/b
+}
+
+function mod(){
+    let a=parseInt(document.getElementById("mod1").value)
+    let b=parseInt(document.getElementById("mod2").value)
+    console.log(a%b);
+    let h3=document.getElementById("h7")
+    h3.innerHTML=a%b
+}
