6055448939611261d323386c78fdc3c25bad2156
fayaass <muhammedfayas59504@gmail.com>
2024-12-11 08:56:36 +0530

    new

--- javascript/demo.js
+++ javascript/demo.js
@@ -132,16 +132,52 @@
 
 
 
-function fun1(){
-    let a=parseInt(document.getElementById("a").value)
-    let b=parseInt(document.getElementById("b").value)
-    let h3=document.getElementById("h1")
-    if (b>=5){
-        s=a*0.05
-        v=a+s
-        h3.innerHTML=v
-        }
-        else{
-        h3.innerHTML=('no bonus')
-        }
+// function fun1(){
+//     let a=parseInt(document.getElementById("a").value)
+//     let b=parseInt(document.getElementById("b").value)
+//     let h3=document.getElementById("h1")
+//     if (b>=5){
+//         s=a*0.05
+//         v=a+s
+//         h3.innerHTML=v
+//         }
+//         else{
+//         h3.innerHTML=('no bonus')
+//         }
+// }
+
+
+let data=[,{id:'1',name:'fayas',age:22},{id:'2',name:'shahid',age:22},{id:'3',name:'althaf',age:22},{id:'4',name:'yasin',age:22},{id:'5',name:'aslam',age:22}]
+
+function Display(){
+    let tbody=document.querySelector("tbody")
+    tbody.innerHTML=''
+    data.forEach(element=>{
+        
+        let tbody=document.querySelector("tbody")
+        let tr = document.createElement("tr")
+        let id_td = document.createElement("td")
+        id_td.innerHTML=element.id
+        tr.appendChild(id_td)
+        let name_td=document.createElement("td")
+        name_td.innerHTML=element.name
+        tr.appendChild(name_td)
+        let age_td=document.createElement("td")
+        age_td.innerHTML=element.age
+        tr.appendChild(age_td)
+        tbody.appendChild(tr)
+
+    })
 }
+
+document.getElementById("add_form").addEventListener('submit',function(event){
+event.preventDefault()
+let id=document.getElementById("id").value
+let name=document.getElementById("name").value
+let age=document.getElementById("age").value
+data.push({id:id,name:name,age:age})
+Display()
+
+})
+
+Display()
